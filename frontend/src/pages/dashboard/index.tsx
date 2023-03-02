0a473c9083bff80762f5a4a99112b2571022b3fe
Matheus Boeira Dias <matheusboeira.aluno@unipampa.edu.br>
2023-03-02 13:03:01 -0300

    few changes

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,16 +1,55 @@
+import styles from './Dashboard.module.scss'
+
 import Header from '@src/components/Header'
 import Head from 'next/head'
+import { FiRefreshCcw } from 'react-icons/fi'
 import { AuthGuard } from '../../utils/AuthGuard'
+import { setupAPI } from '@src/services/api'
+import { useState } from 'react'
 
-const Dashboard = () => {
+interface OrderProps {
+	id: string
+	table: number
+	status: boolean
+	draft: boolean
+	name: string | null
+}
+
+const Dashboard = ({ orders }: { orders: OrderProps[] }) => {
+	const [orderList, setOrderList] = useState(orders)
+
+	const handleModalView = (id: string) => {
+		console.log('id', id)
+	}
+
 	return (
 		<>
 			<Head>
 				<title>Painel - SujeitoPizzaria</title>
 			</Head>
-			<div>
-				<Header />
-			</div>
+			<Header />
+			<main className={styles.container}>
+				<div className={styles.containerHeader}>
+					<h1 className='text-2xl'>Últimos Pedidos</h1>
+					<button>
+						<FiRefreshCcw size={25} color='#3fffa3' />
+					</button>
+				</div>
+				<article className={styles.listOrders}>
+					{orderList.map((order) => (
+						<section
+							key={order.id}
+							className={styles.orderItem}
+							onClick={() => handleModalView(order.id)}
+						>
+							<button>
+								<div className={styles.tag} />
+								<span>Mesa {order.table}</span>
+							</button>
+						</section>
+					))}
+				</article>
+			</main>
 		</>
 	)
 }
@@ -18,7 +57,12 @@ const Dashboard = () => {
 export default Dashboard
 
 export const getServerSideProps = AuthGuard(async (context) => {
+	const api = setupAPI(context)
+	const response = await api.get('/orders')
+
 	return {
-		props: {}
+		props: {
+			orders: response.data
+		}
 	}
 })
